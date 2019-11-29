b5ac898212a3f60aedd64051924c04d66cc23a62
Mohammed Abdel Moneim Basha <mohammedbasha9191@gmail.com>
2019-11-29 19:46:58 +0200

    Editing to publish

--- precache-manifest.eeeeb495e45830a5cc404208779fc6eb.js
+++ precache-manifest.eeeeb495e45830a5cc404208779fc6eb.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "54626138f065ede8fe732282687856ba",
-    "url": "index.html"
+    "url": "/UltraProfile/index.html"
   },
   {
     "revision": "9115784e989863f6eedf",
-    "url": "static/css/main.271d8bfc.chunk.css"
+    "url": "/UltraProfile/static/css/main.271d8bfc.chunk.css"
   },
   {
     "revision": "501e0b7b2fc22a81d568",
-    "url": "static/js/2.989b814d.chunk.js"
+    "url": "/UltraProfile/static/js/2.989b814d.chunk.js"
   },
   {
     "revision": "9115784e989863f6eedf",
-    "url": "static/js/main.140d5a29.chunk.js"
+    "url": "/UltraProfile/static/js/main.140d5a29.chunk.js"
   },
   {
     "revision": "a0fdb89e34f56c854862",
-    "url": "static/js/runtime-main.7c3ea6a4.js"
+    "url": "/UltraProfile/static/js/runtime-main.7c3ea6a4.js"
   }
 ]);
